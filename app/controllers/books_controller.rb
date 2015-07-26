bfb026ad23f3331b79f341159cc4395e1dba2758
Gon Gis <gonzagis87@gmail.com>
2015-07-26 19:54:00 +0000

    List top rated books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,13 @@
 class BooksController < ApplicationController
 	def show
 		books = Book.all
+
+		if rating = params[:rating]
+			books =  books.where(rating: rating)
+		end
+
 		render json: books, status: 200
 	end
+
 
 end
